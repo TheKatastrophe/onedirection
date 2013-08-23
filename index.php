9c048d69e22e15f3ab1c9eb0880f53934351fdf8
Kay Leacock <kay@ccpgsolutions.net>
2013-08-23 09:54:35 +0000

    Added tumblr parsing and 404 function

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <?php
 
 include('settings.php');
+include('include/simple_html_dom.php');
 
-$here = preg_replace("/^".preg_replace("/\//", '\/', preg_replace("/^\//", '', $URLbase))."\//", '', preg_replace("/(?:^\/|\/$)/", '', $_SERVER['REQUEST_URI']));
+$here = preg_replace("/^".preg_replace("/\//", '\/',preg_replace("/(?:^\/|\/$)/", '', $URLbase))."\//", '', preg_replace("/(?:^\/|\/$)/", '', $_SERVER['REQUEST_URI']));
 $url = explode("/", $here);
 
-if($networkes[$url[0]]){
-	
-}else{
+if($networks[$url[0]]){
+	if($networks[$url[0]]['type'] == "tumblr"){
+		$url = $networks[$url[0]]['url']."/post/".$url[1];
+		$page = @file_get_html($url);
+		if(!$page){ fileNotFound(); }
+		$html = $page->find('div.post', 0);
+		$page = str_get_html($html);
+		$attribs = $page->find('a');
+		foreach($attribs as $a){
+			$a->setAttribute('href', '#');
+			$a->setAttribute('target', '');
+		}
 
+		echo $page->innertext;
+	}elseif($networks[$url[0]]['type'] == "twitter"){
+
+	}
+}else{
+	fileNotFound();
+}
+
+function fileNotFound(){
+	header("HTTP/1.1 404 File Not Found");
+	echo "File not found :(";
+	exit();
 }
